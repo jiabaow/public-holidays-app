623411672794a9ed0a77ea7f1348c10a7f49d2ab
jiabaow <jiabaowenwendy@gmail.com>
2025-01-15 10:53:02 -0800

    finish basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [country, setCountry] = useState('DE');
+  const [holidaysData, setHolidaysData] = useState([]);
+  const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        console.log(`Country changed to: ${country}`);
+        fetch(`https://openholidaysapi.org/PublicHolidays?countryIsoCode=${country}&validFrom=2025-01-01&validTo=2025-12-31&languageIsoCode=EN`)
+            .then((res) => res.json())
+            .then((data) => setHolidaysData(data));
+    }, [country]);
+
+    useEffect(() => {
+        fetch(`https://openholidaysapi.org/Countries?languageIsoCode=EN`)
+            .then((res) => res.json())
+            .then((data) => {
+                const countryOptions = data.map((item) => ({
+                    value: item.isoCode,
+                    label: item.name.find((n) => n.language === 'EN') ?.text || item.isoCode,
+                }));
+                setOptions(countryOptions);
+            })
+    },[]);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="dropdown">
+          <label htmlFor="country-select">Select Country:</label>
+          <select
+              id="country-select"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}>
+              {options.map((option) => (
+                  <option key={option.value} value={option.value}>
+                      {option.label}
+                  </option>
+              ))}
+          </select>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+          {holidaysData.map((item) => {
+              const enName = item.name.find((n) => n.language === 'EN');
+              return <li key={item.id}>{item.startDate} - {enName.text}</li>;
+          })}
+      </ul>
     </>
   )
 }
